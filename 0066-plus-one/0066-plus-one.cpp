d62c91e9adb76c41d6a6040d38c3e0ccb92f046d
Siddharth Meur <siddharthmeur18@gmail.com>
2026-08-10 09:53:23 +0530

    Time: 0 ms (100%), Space: 11.5 MB (62.54%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
         for(int i=digits.size()-1;i>=0;i--){
-            if(digits[i]<9){
-                digits[i]++;
+            if(digits[i]<9){   //1
+                digits[i]++;    // flow is from 1st to 2nd then repeat if no return digit from 1 then go in 3rd
                 return digits;
             }
-            if(digits[i]==9){
+            if(digits[i]==9){  //2
                 digits[i]=0;
             }
         }
-            digits.insert(digits.begin(), 1);
+            digits.insert(digits.begin(), 1);  //3
             return digits;
     }
     
